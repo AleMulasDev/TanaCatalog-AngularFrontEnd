ae2fe34773eb4556dd8bb35f073596fb0518b5be
BattleIsOver <34491874+BattleIsOver@users.noreply.github.com>
2020-04-03 18:31:44 +0200

    Added new endpoints
    checktoken
    gamepermission
    section game
    section permission
    section user

--- src/app/_utils/constants.ts
+++ src/app/_utils/constants.ts
@@ -2,6 +2,7 @@ const SERVER_ADDRESS  = `http://localhost:4000`;
 const BASE_PATH = `${SERVER_ADDRESS}/api/tana`;
 const BASE_PATH_USERS = `${BASE_PATH}/users`;
 const LOGIN_PATH = `${BASE_PATH_USERS}/login`;
+const TOKEN_CHECK_PATH = `${BASE_PATH_USERS}/checkToken`
 const REGISTER_PATH = `${BASE_PATH_USERS}/register`;
 const RECOVER_PATH = `${BASE_PATH_USERS}/recover`;
 const VERIFY_PATH = `${BASE_PATH_USERS}/verify`;
@@ -11,9 +12,13 @@ const CHANGE_EMAIL_PATH = `${BASE_PATH_USERS}/changeEmail`;
 const BASE_PATH_GAMES = `${BASE_PATH}/games`;
 const SEARCHBGG_GAME_PATH = `${BASE_PATH}/searchBgg`;
 const FETCHBGG_GAME_PATH = `${BASE_PATH}/fetchBgg`;
+const GAME_PERMISSION_PATH = `${BASE_PATH_GAMES}/permission`;
 
 const BASE_PATH_SECTIONS = `${BASE_PATH}/sections`;
 const HOLDER_PATH = `${BASE_PATH_SECTIONS}/holder`;
+const SECTION_GAMES_PATH = `${BASE_PATH_SECTIONS}/game`;
+const SECTION_PERMISSION_PATH = `${BASE_PATH_SECTIONS}/permission`;
+const SECTION_USER_PATH = `${BASE_PATH_SECTIONS}/user`;
 
 const APP_ADDRESS = `http://localhost:4200`;
 const APP_LOGIN_PATH = `${APP_ADDRESS}/login`;
@@ -29,6 +34,7 @@ export const constant  = {
     BASE_PATH,
     BASE_PATH_USERS,
     LOGIN_PATH,
+    TOKEN_CHECK_PATH,
     REGISTER_PATH,
     RECOVER_PATH,
     VERIFY_PATH,
@@ -37,8 +43,12 @@ export const constant  = {
     BASE_PATH_GAMES,
     SEARCHBGG_GAME_PATH,
     FETCHBGG_GAME_PATH,
+    GAME_PERMISSION_PATH,
     BASE_PATH_SECTIONS,
     HOLDER_PATH,
+    SECTION_GAMES_PATH,
+    SECTION_PERMISSION_PATH,
+    SECTION_USER_PATH
   },
   client: {
     APP_ADDRESS,
